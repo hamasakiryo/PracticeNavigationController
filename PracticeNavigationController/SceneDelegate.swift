18d5cdc3fa06555b53286541b0d4d8acad487b40
hamasakiryo <hamaan.0927@icloud.com>
2024-02-06 17:04:51 +0900

    rootViewControllerをFirstViewControllerに変更

--- PracticeNavigationController/SceneDelegate.swift
+++ PracticeNavigationController/SceneDelegate.swift
@@ -13,10 +13,14 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
-        // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
-        // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        guard let _ = (scene as? UIWindowScene) else { return }
+        // guard let _　をwindowSceneに書き換え
+                guard let windowScene = (scene as? UIWindowScene) else { return }
+                //Windowをインスタンス化
+                window = UIWindow(windowScene: windowScene)
+                //rootViewControllerにStoryboardをHomeViewに指定して初期ビューコントローラーをインスタンス化
+                window?.rootViewController = UIStoryboard(name: "First", bundle: nil).instantiateInitialViewController() as! FirstViewController
+                //ウィンドウ表示を実行
+                window?.makeKeyAndVisible()
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
