93abb9a2e7e64058b202f9d55b938e886ae9a3ca
hamasakiryo <hamaan.0927@icloud.com>
2024-02-06 16:03:04 +0900

    initial commit

---
+++ PracticeNavigationController/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PracticeNavigationController
+//
+//  Created by 浜崎良 on 2024/02/06.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
